24cad30abdcf051dd6eb5b143f7e6dcd4cd5145f
maciel-j <jose.s.maciel@outlook.com>
2024-09-26 08:14:09 -0400

    module emotion_detection 2024.09.26

--- emotion_detection.py
+++ emotion_detection.py
@@ -15,4 +15,24 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url, json = input_json, headers=headers, timeout=10)
 
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    anger_score =  formatted_response['emotionPredictions'][0]["emotion"]["anger"]
+    disgust_score = formatted_response['emotionPredictions'][0]["emotion"]["disgust"]
+    fear_score = formatted_response['emotionPredictions'][0]["emotion"]["fear"]
+    joy_score = formatted_response['emotionPredictions'][0]["emotion"]["joy"]
+    sadness_score = formatted_response['emotionPredictions'][0]["emotion"]["sadness"]
+
+    dominant_emotion = max(
+        formatted_response['emotionPredictions'][0]["emotion"],
+        key=formatted_response['emotionPredictions'][0]["emotion"].get
+    )
+
+    return     {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
